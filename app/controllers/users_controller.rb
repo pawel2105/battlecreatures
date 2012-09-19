1e1fe3235a266c95e66aaab6a016e5e05d77482f
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-09-19 23:12:34 +0200

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :login_required
+
   def show
     @user = User.find_by_id(params[:id]) || current_user
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,26 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+  context "functionality" do
+
+    before :each do
+      @current_user = create(:user)
+      controller.stub(:current_user).and_return(@current_user)
     end
+
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show'
+        response.should be_success
+      end
+
+      it "assigns the requested user as @user" do
+        get 'show'
+        assigns(:user).should eq(@current_user)
+      end
+
+    end
+
   end
 
 end
